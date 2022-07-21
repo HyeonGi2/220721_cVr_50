bc03dff9292e5c0eca2be4525e433e110e533df7
HyeonGi2 <tmskc56@gmail.com>
2022-07-21 09:27:36 +0900

    마이바티스, MySQL JDBC 드라이버 추가, 설정 파일에 DB접속정보 기입

---
+++ src/main/java/com/sbs/exam/day1/repository/ArticleRepository.java
@@ -0,0 +1,24 @@
+package com.sbs.exam.day1.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.sbs.exam.day1.vo.Article;
+
+@Mapper
+public interface ArticleRepository {
+
+	public Article writeArticle(String title, String body);
+
+	@Select("select * from article where id = #{id}")
+	public Article getArticle(@Param("id") int id);
+
+	public void deleteArticle(int id);
+
+	public void modifyArticle(int id, String title, String body);
+
+	public List<Article> getArticles();
+}
